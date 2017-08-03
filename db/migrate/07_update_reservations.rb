763952c65884f8c18d1aa90f9f7452cfd559740d
Tim Freeman <d.timothy.freeman@gmail.com>
2017-08-03 17:02:10 -0400

    done

---
+++ db/migrate/07_update_reservations.rb
@@ -0,0 +1,8 @@
+class UpdateReservations < ActiveRecord::Migration
+	def change
+		change_column :reservations, :checkin, :datetime
+		change_column :reservations, :checkout, :datetime
+	end
+
+
+end
